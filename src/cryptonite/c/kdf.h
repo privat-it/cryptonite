a52e745e0a493cddd20702e65df6e53ac8718d59
Mykola Baibuz <kolrbb@gmail.com>
2019-09-14 17:59:26 +0300

    Подправил форматирование

--- src/cryptonite/c/kdf.h
+++ src/cryptonite/c/kdf.h
@@ -8,16 +8,16 @@ extern "C" {
 #endif
 
 typedef enum Pbkdf2HmacId_st {
-	PBKDF2_GOST_HMAC_ID = 0,
-	PBKDF2_SHA1_HMAC_ID = 1,
-	PBKDF2_SHA224_HMAC_ID = 2,
-	PBKDF2_SHA256_HMAC_ID = 3,
-	PBKDF2_SHA384_HMAC_ID = 4,
-	PBKDF2_SHA512_HMAC_ID = 5
+    PBKDF2_GOST_HMAC_ID = 0,
+    PBKDF2_SHA1_HMAC_ID = 1,
+    PBKDF2_SHA224_HMAC_ID = 2,
+    PBKDF2_SHA256_HMAC_ID = 3,
+    PBKDF2_SHA384_HMAC_ID = 4,
+    PBKDF2_SHA512_HMAC_ID = 5
 } Pbkdf2HmacId;
 
-CRYPTONITE_EXPORT int kdf_pbkdf2(const char* pass, const ByteArray* salt, unsigned long iterations, 
-                                 size_t key_len, Pbkdf2HmacId id, ByteArray** dk);
+CRYPTONITE_EXPORT int kdf_pbkdf2(const char *pass, const ByteArray *salt, unsigned long iterations,
+        size_t key_len, Pbkdf2HmacId id, ByteArray **dk);
 
 #ifdef  __cplusplus
 }
