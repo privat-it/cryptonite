f5f2debb253c079dd7f245a7699843abe9c1f553
Vadim <v.shevchuk@monobank.com.ua>
2021-03-12 13:03:05 +0200

    fix header compile c++

--- src/cryptonite/c/paddings.h
+++ src/cryptonite/c/paddings.h
@@ -3,6 +3,10 @@
 
 #include "byte_array.h"
 
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
 CRYPTONITE_EXPORT int make_pkcs7_padding(const ByteArray *data, uint8_t block_len, ByteArray **data_with_padding);
 
 CRYPTONITE_EXPORT int make_pkcs7_unpadding(const ByteArray *data_with_padding, ByteArray **data_without_padding);
@@ -10,5 +14,9 @@ CRYPTONITE_EXPORT int make_pkcs7_unpadding(const ByteArray *data_with_padding, B
 CRYPTONITE_EXPORT int make_iso_7816_4_padding(const ByteArray *data, uint8_t block_len, ByteArray **data_with_padding);
 
 CRYPTONITE_EXPORT int make_iso_7816_4_unpadding(const ByteArray *data_with_padding, ByteArray **data_without_padding);
+
+#ifdef  __cplusplus
+}
+#endif
 
 #endif //CRYPTONITE_PADDINGS_H
